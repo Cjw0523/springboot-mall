7c57cc90170a4c6feeb6c9e60a34b13195b2ec9f
chenjinwang <pnn0908128305@gmail.com>
2023-10-30 21:45:04 +0800

    初始化 Spring Boot project

---
+++ springboot-mail/src/test/java/com/chenjinwang/springbootmail/SpringbootMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenjinwang.springbootmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
